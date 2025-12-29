72173deff59bfc508ff945b96136b68e6b3f9637
maurya-dhananjay <dhananjaymaury366@gmail.com>
2025-12-29 18:12:59 +0530

    Remove comments from app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-# Load model
+
 model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(
     model_id,
@@ -9,13 +9,13 @@ pipe = StableDiffusionPipeline.from_pretrained(
 )
 pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
-# Text prompt
+
 prompt = "A futuristic robot working on a laptop"
 
-# Generate image
+
 image = pipe(prompt).images[0]
 
-# Save image
+
 image.save("outputs/generated_image.png")
 
 print("Image generated successfully!")
